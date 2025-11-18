e81e1dedbd715eb948e2e8c0a2563b68662e3933
casvanluijtelaar <cas.luijtelaar@umain.com>
2025-11-18 11:10:32 +0100

    update signature

--- src/main.ts
+++ src/main.ts
@@ -27,11 +27,9 @@ const content = `
 
     <h2>But ideally, shoot me a <a href="mailto:casvanluijtelaar@hotmail.com">message</a>. so we can hop on a call, or even better, grab a coffee.</h2>
 
-    <p>
-      Cas van Luijtelaar<br>
-      <small>Tech lead @ <a href="https://umain.com/" target="_blank">Umain</a></small><br>
-      <small>Stockholm</small>
-    </p>
+    <p>Cas van Luijtelaar</p>
+    <p><small>Tech lead @ <a href="https://umain.com/" target="_blank">Umain</a></small></p>
+    <p><small>Stockholm</small></p>
 `;
 
 function typeWriter() {
