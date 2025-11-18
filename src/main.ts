9d660f5f685e4961a125f9709d89ad13ea6a58a3
casvanluijtelaar <cas.luijtelaar@umain.com>
2025-11-18 16:15:19 +0100

    fix typos

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,19 @@ import './style.css';
 
 const typedContentDiv = document.querySelector<HTMLDivElement>('#app');
 const content = `
-    <h2>Software development consultant focussing on helping teams and clients scale software not people. Mainly been deep diving into building and scaling modern mobile solutions.</h2>
+    <h2>Software development consultant focusing on helping teams and clients scale software not people. Mainly been deep diving into building and scaling modern mobile solutions.</h2>
 
     <h3>I enjoy sharing learnings from big projects and clients</h3>
     <ul>
       <li><a href="https://www.youtube.com/watch?v=zDRlEp7r5i0" target="_blank">Building a 45 million MOA app from scratch</a></li>      
-      <li><a href=" https://evolutionjobs.com/exchange/evo-nordics-419-kotlin-multi-platform-discussion/" target="_blank">The power of the cross platform domain layer</a></li>      
+      <li><a href="https://evolutionjobs.com/exchange/evo-nordics-419-kotlin-multi-platform-discussion/" target="_blank">The power of the cross platform domain layer</a></li>      
       <li><a href="https://www.youtube.com/watch?v=HSIhkB5bGJs" target="_blank">Cross platform navigation patterns and system</a></li>
     </ul>
 
     <h3>Complex solutions for a specific project should ideally be abstracted and shareable</h3>
     <ul>
        <li><a href="https://github.com/apegroup/revolver" target="_blank">Cross platform state management</a></li>	
-       <li><a href="https://github.com/casvanluijtelaar/reorderable_grid" target="_blank">Scaleable, reorderable, animated grid framework</a></li>	
+       <li><a href="https://github.com/casvanluijtelaar/reorderable_grid" target="_blank">Scalable, reorderable, animated grid framework</a></li>	
        <li><a href="https://github.com/casvanluijtelaar/faker.dart" target="_blank">Generate massive amounts of mock data</a>, pre-LLMs 😉</li>	
        <li><a href="https://github.com/casvanluijtelaar/paged_vertical_calendar" target="_blank">Let me solve the headache of calendar infrastructure for you</a></li>
     </ul>
